dc51a904ef5d80bc59e34cb9afe19b672e1c4fc2
Cíntia Arantes <114604292+cintiaarantes@users.noreply.github.com>
2023-09-24 13:19:24 -0300

    Add files via upload

---
+++ config.js
@@ -0,0 +1,10 @@
+import firebase from "firebase";
+require("@firebase/firestore");
+
+const firebaseConfig = {
+   
+};
+
+firebase.initializeApp(firebaseConfig);
+
+export default firebase.firestore();
